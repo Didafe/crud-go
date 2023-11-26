68d6a869296e4c3df3590f528486e13d9a2f35f8
didafe <you@example.com>
2023-11-26 15:10:32 -0300

    Fix bug

--- src/controller/test.go
+++ src/controller/test.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import "github.com/Didafe/crud-go/crud-go/src/configuration/rest_err"
 
@@ -6,4 +6,17 @@ func test(message string) (err *rest_err.RestErr) {
 
 	err.Code
 
+}*/
+
+package controller
+
+import "github.com/Didafe/crud-go/crud-go/src/configuration/rest_err"
+
+func test(message string) (err *rest_err.RestErr) {
+	if err != nil {
+		// Se `err` não for nulo, então você pode acessar a propriedade `Code`
+		_ = err.Code
+	}
+	return err
 }
+
